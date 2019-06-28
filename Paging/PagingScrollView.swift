cfccbb2de3808532d12033bf532e200fcc78cde1
Squirrel <songtao046@126.com>
2019-06-28 17:20:03 +0800

    initial

---
+++ Paging/PagingScrollView.swift
@@ -0,0 +1,13 @@
+//
+//  PagingScrollView.swift
+//  Paging
+//
+//  Created by Squirrel on 2019/6/28.
+//  Copyright © 2019 Squirrel. All rights reserved.
+//
+
+import UIKit
+
+class PagingScrollView: UIScrollView {
+
+}
---
+++ Paging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Paging
+//
+//  Created by Squirrel on 2019/6/28.
+//  Copyright © 2019 Squirrel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
